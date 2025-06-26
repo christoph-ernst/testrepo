a3114b12246ff6aea255b9c2d561fa50bd007e77
christoph-ernst <github@ernst.ch>
2025-06-26 16:16:48 +0200

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,3 @@
+aws_access_key = "AKIA1234567890EXAMPLE"
+aws_secret_access_key = wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+aws_session_token = AQoEXAMPLEH4aoAH0gNCAPy...truncated...zrkuWJOgQs8IZZaIv2BXIa2R4Olgk
